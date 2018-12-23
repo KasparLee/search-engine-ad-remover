a4354d4866339508ae5082d649b3e20c4f5a69df
Kaspar Lee <personaldev@kasparlee.com>
2018-12-23 00:07:52 +0000

    Add all existing code

---
+++ src/scripts/se/ask.js
@@ -0,0 +1,58 @@
+///////////////
+// Main Code //
+///////////////
+
+	var prefs
+
+	chrome.runtime.sendMessage({'id': 'get_prefs'}, function(se_prefs) {
+		prefs = se_prefs['ask']
+		// Add Stylesheet if not already added
+			if ($('#SEAR_stylesheet').length < 1) {
+				main()
+			}
+	})
+
+	function main() {
+		// Initiate stylesheet
+	    	var stylesheet = '\
+	    		\n\
+				/************************************************************/\n\
+				/*                                                          */\n\
+				/* SearchEngineAdRemover (Addon) - Stylesheet to remove ads */\n\
+				/*                                                          */\n\
+				/************************************************************/\n\
+				\n'
+
+		// Above Search Results
+			if (prefs['above']) {
+				stylesheet = stylesheet + '\
+					/* Above Search Results */\n\
+					#magic1 { display: none !important; }\n\
+					\n'
+			}
+		// Below Search Results
+			if (prefs['below']) {
+				stylesheet = stylesheet + '\
+					/* Below Search Results */\n\
+					#magic2 { display: none !important; }\n\
+					\n'
+			}
+		// "Related Searches" section
+			if (prefs['r_searches']) {
+				stylesheet = stylesheet + '\
+					/* "Related Searches" section (Search) */\n\
+					.related-search-top { display: none !important; }\n\
+					#mrRSM { display: none !important; }\n\
+					\n'
+			}
+		// "Shopping results" section
+			if (prefs['s_results']) {
+				stylesheet = stylesheet + '\
+					/* "Shopping results" section (Search) */\n\
+					#lcs-smart-answer { display: none !important; }\n\
+					\n'
+			}
+
+        // Attach Stylesheet
+        	$('<style id="SEAR_stylesheet">').text(stylesheet).appendTo('head')
+	}
